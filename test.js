a454d962987b00a5b2150ac777ae34734c0f9796
qizhaoyin <qizhaoyin@baidu.com>
2026-04-14 09:47:47 +0800

    懂车帝

--- test.js
+++ test.js
@@ -1,33 +1,11 @@
-// 防抖
-function debounce(fn, delay) {
-    let timer = null;
-    return function() {
-        console.log('1111')
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            fn.apply(this, arguments);
-        }, delay)
-    }
+async function asc2() {
+    console.log('1222');
 }
 
-// function a1() {
-//     console.log(new Date().getSeconds());
-// }
-
-// const deFunction = () => {
-//     debounce(a1, 2000)();
-// }
-// deFunction();
-// deFunction();
-// deFunction();
-// deFunction();
-
-// const debounceFn = debounce(() => {
-//     console.log('执行了')
-// }, 2000)
-// debounceFn()
-// debounceFn()
-// debounceFn()
-// setTimeout(()=> {
-//     debounceFn();
-// }, 1000)
+async function asc1() {
+    console.log('112222')
+    await asc2();
+    console.log(111);
+}
+asc1();
+console.log('hahha')
